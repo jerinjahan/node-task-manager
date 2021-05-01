240a273374d2c7d52086d19fc8d58830e8cc8bad
Jerin Jahan <crytekredhat@gmail.com>
2021-05-01 14:53:10 +0600

    added cloudinary and multer function

--- models/category.model.js
+++ models/category.model.js
@@ -8,22 +8,3 @@ const Category = mongoose.model(
 );
 
 module.exports = Category;
-
-
-// module.exports = mongoose => {
-//     var schema = mongoose.Schema(
-//         {
-//             name: String,
-//         },
-//         { timestamps: true }
-//     );
-
-//     schema.method("toJSON", function() {
-//         const { __v, _id, ...object } = this.toObject();
-//         object.id = _id;
-//         return object;
-//     });
-
-//     const Category = mongoose.model("category", schema);
-//     return Category;
-// };
